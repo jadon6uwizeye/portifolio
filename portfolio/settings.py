8b96b5414805d0c8c9f05b8bd328a6fe8dbad327
Uwizeye Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2022-10-26 16:17:35 +0200

    fix static files issue

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&9wvd)j27f^8%f+dud(-f0vxau*1avo_+b2n$t8b30%0kr@%hr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
